3a1ce49604c549bc4baaa00ba05e35497b946e60
Simon Vinding Brodersen <simon@3450.dk>
2024-10-07 15:37:40 +0200

    Work on excercise

--- week5/handout/src/APL/Tests.hs
+++ week5/handout/src/APL/Tests.hs
@@ -1,10 +1,23 @@
 module APL.Tests where
 
 import APL.AST (Exp (..), VName)
-import Test.QuickCheck (Gen)
+import Test.QuickCheck (Gen, elements, listOf, oneof, sample, sized)
 
 genVar :: Gen VName
-genVar = undefined
+genVar = do
+  alpha <- elements ['a' .. 'z']
+  alphaNums <- listOf $ elements $ ['a' .. 'z'] ++ ['0' .. '9']
+  pure (alpha : alphaNums)
 
-genExp :: Gen Exp
-genExp = undefined
+genExp :: Int -> Gen Exp
+genExp n =
+  if n < 1
+    then
+      Var <$> genVar
+    else
+      let half = (n - 1) `div` 2
+       in oneof
+            [ Lambda <$> genVar <*> genExp (n - 1)
+            , Var <$> genVar
+            , Apply <$> genExp half <*> genExp half
+            ]
